f5c3111ed5b74076b4dd8c7828fb678f95203966
Maurici Yagües <mauriciyagues@gmail.com>
2022-03-07 18:23:14 +0100

    Read URLs from file

--- reading_list.js
+++ reading_list.js
@@ -1,43 +1,44 @@
-
-const Readability = require('readability'),
-  fs = require('fs'),
+const { Readability } = require("@mozilla/readability"),
+  fs = require("fs"),
   { JSDOM } = require("jsdom"),
-  now = new Date(),
-  url_list = [
-    'https://grafik.agency/insight/waveofthefuture',
-    'https://www.vice.com/en/article/xwqjg3/the-complete-untold-history-of-halo-an-oral-history',
-  ];
+  url_list = fs.readFileSync("reading_list.txt", "utf-8").trim().split("\n");
 
-const getText = url => {
+const getText = (url) => {
   return new Promise((resolve, reject) => {
     try {
-      const htmlObject = JSDOM.fromURL(url).then(dom => {
+      const htmlObject = JSDOM.fromURL(url).then((dom) => {
         return dom.serialize();
       });
       resolve(htmlObject);
-    }
-    catch(error) {
+    } catch (error) {
       reject(error);
     }
   });
 };
 
-let web_content = "<html><head><meta charset='utf-8'><title>Reading list</title></head>";
-(async function(){
-  await Promise.all(url_list.map(async (url) => {
-    await getText(url).then(webText => {
-      let dom = new JSDOM(webText, {features: {resources: "usable"}});
-      let article = new Readability(dom.window.document).parse();
-      web_content += ('<h1>' + article.title + '</h1>' + article.content + '\n');
-    }, err => {
-      console.log(err);
-    });
-  }));
-  console.log('Webpages processed!')
+let web_content =
+  "<html><head><meta charset='utf-8'><title>Reading list</title></head>";
+(async function () {
+  await Promise.all(
+    url_list.map(async (url) => {
+      await getText(url).then(
+        (webText) => {
+          let dom = new JSDOM(webText, { features: { resources: "usable" } });
+          let article = new Readability(dom.window.document).parse();
+          web_content +=
+            "<h1>" + article.title + "</h1>" + article.content + "\n";
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    })
+  );
+  console.log("Webpages processed!");
   // Set callback function
   callback = function (err, _) {
-    if (err) console.error(err)};
-    // return console.log(result), result};
-  fs.writeFile(now.toDateString().replace(/\s/g, '_') + '.html', web_content, callback)
+    if (err) console.error(err);
+  };
+  // return console.log(result), result};
+  fs.writeFile("reading_list.html", web_content, callback);
 })();
-
